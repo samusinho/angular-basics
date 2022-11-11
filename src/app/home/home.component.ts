49bbfc409b1fe86c7d57f4e8ed1b2bb43ca85e6e
Samuel Pérez <samusinho79@gmail.com>
2022-11-10 19:42:53 -0500

    binding - interpolacion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
   constructor() { }
+  title = "Frutas y verduras"
+  btnText = "Mostrar"
+  infoText = "la información está oculta"
+  infoHidden = true;
 
   ngOnInit(): void {
+  }
+
+  onClickButton() {
+    this.infoHidden = !this.infoHidden;
+    this.btnText = this.infoHidden ? "Mostrar" : "Ocultar"
+    this.infoText = this.infoHidden ? "la información está oculta" : "La información ya no está oculta"
   }
 
 }
