7c74117639e8888485003ca3444565d00b55ae8b
Samuel Pérez <samusinho79@gmail.com>
2022-11-10 20:49:12 -0500

    directiva ngFor

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,6 +9,20 @@ export class ProductsComponent implements OnInit {
 
   constructor() { }
 
+  products = [{
+    name: "Plátano verde",
+    price: 1000,
+    img: 'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/09/conservar-platanos-verdes-luisecheverriurrea.jpg'
+  }, {
+    name: "Papita criolla",
+    price: 2000,
+    img: 'https://cdn10.totalcode.net/mercacentro/product-zoom/es/papa-criolla-kilo-1000-g-1.webp'
+  }, {
+    name: "Apio españa",
+    price: 1500,
+    img: 'https://mejorconsalud.as.com/wp-content/uploads/2018/05/apio-fresco-cortado-entero.jpg'
+  }]
+
   ngOnInit(): void {
   }
 
